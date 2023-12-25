f806fef5c528ea301a195c6b8b2d0fe57f1d31af
Pratiksha Bramhane <pbramhane43@gmail.com>
2023-12-25 12:52:39 +0530

    created Profile model

--- weConnect/weConnectApp/models.py
+++ weConnect/weConnectApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key = True,on_delete=models.CASCADE)
+    name = models.CharField(max_length =30, blank = True, null = True)
+    about = models.TextField(max_length = 200, blank=True)
+    profilePic = models.ImageField(upload_to='profilePic', default=None)
+    DOB = models.DateField()

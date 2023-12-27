b2d25722cda8071871a09c2370bb31088bb0f6fd
Prerna3 <prerna.ranjan@slrtce.in>
2023-12-27 16:18:02 +0530

    changes

--- weConnect/weConnectApp/views.py
+++ weConnect/weConnectApp/views.py
@@ -3,61 +3,64 @@ from .forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from . models import Profile, Post, LikePost, FollowersCount
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your views here.
 
+
 def register(request):
     form = CreateUserForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('mylogin')
-    
-    context = {'registerform': form}
+            return redirect("mylogin")
 
-    return render(request, 'register.html', context)
+    context = {"registerform": form}
+
+    return render(request, "register.html", context)
+
 
 def myLogin(request):
     form = LoginForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+            username = request.POST.get("username")
+            password = request.POST.get("password")
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                return redirect('home')
-    
-    context = {'loginform': form}
+                return redirect("home")
 
-    return render(request, 'mylogin.html', context)
+    context = {"loginform": form}
 
-@login_required(login_url='mylogin')
+    return render(request, "mylogin.html", context)
+
+
+@login_required(login_url="mylogin")
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts = Post.objects.all().order_by("-created_at")
     users = User.objects.all()
     adminuser = User.objects.filter(is_superuser=False)
     context = {
-        'posts': posts,
-        'users': users,
-        'adminuser': adminuser,
+        "posts": posts,
+        "users": users,
+        "adminuser": adminuser,
     }
 
-    return render(request, 'home.html', context)
+    return render(request, "home.html", context)
 
 
-@login_required(login_url='mylogin')
+@login_required(login_url="mylogin")
 def myLogout(request):
     auth.logout(request)
-    return redirect('mylogin')
+    return redirect("mylogin")
 
 
-@login_required(login_url='mylogin')
+@login_required(login_url="mylogin")
 def profile(request, user_id):
     profile_object = get_object_or_404(User, pk=user_id)
     profile = Profile.objects.get(user=profile_object)
@@ -66,88 +69,92 @@ def profile(request, user_id):
     numberOfPosts = len(userposts)
     followers_count = FollowersCount.objects.filter(user=profile_object).count()
     following_count = FollowersCount.objects.filter(follower=profile_object).count()
-    
+
     current_user = request.user
-    is_current_user = (current_user == user)
+    is_current_user = current_user == user
     is_following_self = current_user == user
-    is_following = FollowersCount.objects.filter(follower=current_user.username, user=user.username).exists()
+    is_following = FollowersCount.objects.filter(
+        follower=current_user.username, user=user.username
+    ).exists()
 
     context = {
-        'user': user,
-        'profile_object': profile_object,
-        'profile': profile,
-        'userposts': userposts,
-        'numberOfPosts': numberOfPosts,
-        'is_following_self': is_following_self,
-        'is_current_user': is_current_user,
-        'is_following': is_following,
-        'followers_count': followers_count,
-        'following_count': following_count,
+        "user": user,
+        "profile_object": profile_object,
+        "profile": profile,
+        "userposts": userposts,
+        "numberOfPosts": numberOfPosts,
+        "is_following_self": is_following_self,
+        "is_current_user": is_current_user,
+        "is_following": is_following,
+        "followers_count": followers_count,
+        "following_count": following_count,
     }
-    return render(request, 'profile.html', context)
+    return render(request, "Profile.html", context)
 
 
-@login_required(login_url='mylogin')
+@login_required(login_url="mylogin")
 def createProfile(request):
     user = request.user
     profile_exists = Profile.objects.filter(user=user).exists()
     if not profile_exists:
-        if request.method == 'POST':
+        if request.method == "POST":
             user = request.user
-            name = request.POST['name']
-            about = request.POST['about']
-            profilepic = request.FILES['profilePic']
-            DOB = request.POST['DOB']
-            Profile.objects.create(user=user, name=name, about=about, profilePic=profilepic, DOB=DOB)
-            return redirect('profile', user_id=user.id) 
+            name = request.POST["name"]
+            about = request.POST["about"]
+            profilepic = request.FILES["profilePic"]
+            DOB = request.POST["DOB"]
+            Profile.objects.create(
+                user=user, name=name, about=about, profilePic=profilepic, DOB=DOB
+            )
+            return redirect("profile", user_id=user.id)
         else:
-            return render(request, 'createProfile.html')
+            return render(request, "createProfile.html")
     else:
-        return redirect('profile', user_id=user.id) 
+        return redirect("profile", user_id=user.id)
 
 
-@login_required(login_url='mylogin')
+@login_required(login_url="mylogin")
 def editProfile(request, user):
-        user = request.user
-        profile = get_object_or_404(Profile, user=user)
+    user = request.user
+    profile = get_object_or_404(Profile, user=user)
 
-        if request.method == 'POST':
-            name = request.POST['name']
-            about = request.POST['about']
-            profilePic = request.FILES.get('profilePic')
-            DOB = request.POST['DOB']
+    if request.method == "POST":
+        name = request.POST["name"]
+        about = request.POST["about"]
+        profilePic = request.FILES.get("profilePic")
+        DOB = request.POST["DOB"]
 
-            if name:
-                profile.name = name
-            if about:
-                profile.about = about
-            if profilePic:
-                profile.profilePic = profilePic
-            if DOB:
-                profile.DOB = DOB
+        if name:
+            profile.name = name
+        if about:
+            profile.about = about
+        if profilePic:
+            profile.profilePic = profilePic
+        if DOB:
+            profile.DOB = DOB
 
-            profile.save()
-            return redirect('profile', user_id=user.id)
+        profile.save()
+        return redirect("profile", user_id=user.id)
 
-        return render(request, 'editProfile.html', {'profile': profile})
+    return render(request, "editProfile.html", {"profile": profile})
 
 
-@login_required(login_url='mylogin')
+@login_required(login_url="mylogin")
 def addPost(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user = request.user
-        image = request.FILES['image']
-        caption = request.POST['caption']
+        image = request.FILES["image"]
+        caption = request.POST["caption"]
         Post.objects.create(user=user, image=image, caption=caption)
-        return redirect('home')
+        return redirect("home")
     else:
-        return render(request, 'addPost.html')
-    
+        return render(request, "addPost.html")
 
-@login_required(login_url='mylogin')
+
+@login_required(login_url="mylogin")
 def like_post(request):
     username = request.user.username
-    post_id = request.GET.get('post_id')
+    post_id = request.GET.get("post_id")
 
     post = Post.objects.get(id=post_id)
 
@@ -156,33 +163,14 @@ def like_post(request):
     if like_filter == None:
         new_like = LikePost.objects.create(post_id=post_id, username=username)
         new_like.save()
-        post.no_of_likes = post.no_of_likes+1
+        post.no_of_likes = post.no_of_likes + 1
         post.save()
-        return redirect('home')
+        return redirect("home")
     else:
         like_filter.delete()
-        post.no_of_likes = post.no_of_likes-1
+        post.no_of_likes = post.no_of_likes - 1
         post.save()
-        return redirect('home')
-    
-'''
-@login_required(login_url='mylogin')
-def follow(request):
-    if request.method == 'POST':
-        follower = request.POST['follower']
-        user = request.POST['user']
-
-        if FollowersCount.objects.filter(follower=follower, user=user).first():
-            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
-            delete_follower.delete()
-            return redirect('profile', user=request.user)
-        else:
-            new_follower = FollowersCount.objects.create(follower=follower, user=user)
-            new_follower.save()
-            return redirect('profile', user=request.user)
-    
-    else:
-        return redirect('home')'''
+        return redirect("home")
 
 
 @login_required
@@ -190,11 +178,14 @@ def follow(request, user_id):
     followed_user = get_object_or_404(User, pk=user_id)
     if request.user != followed_user:  # Prevent self-following
         FollowersCount.objects.get_or_create(user=followed_user, follower=request.user)
-    return redirect('profile', user_id=user_id)  # Redirect to the user's profile page
+    return redirect("profile", user_id=user_id)  # Redirect to the user's profile page
+
 
 @login_required
 def unfollow(request, user_id):
     unfollowed_user = get_object_or_404(User, pk=user_id)
     if request.user != unfollowed_user:  # Prevent self-unfollowing
-        FollowersCount.objects.filter(user=unfollowed_user, follower=request.user).delete()
-    return redirect('profile', user_id=user_id) 
+        FollowersCount.objects.filter(
+            user=unfollowed_user, follower=request.user
+        ).delete()
+    return redirect("profile", user_id=user_id)
